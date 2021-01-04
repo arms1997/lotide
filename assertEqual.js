7c43441c29d77c7eb755e60a6ba3489bde3e4777
arms1997 <armeen.hadizadeh@gmail.com>
2021-01-04 19:02:31 +0000

    created the assert equal function

---
+++ assertEqual.js
@@ -0,0 +1,8 @@
+const assertEqual = (actual, expected) => {
+  actual === expected ? console.log(`🤑🤑🤑Assertion Passed: ${actual} === ${expected}`) :
+    console.log(`😡😡😡Assertion Failed: ${actual} !== ${expected}`);
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(1, '1');
