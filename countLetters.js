cd003c52238efe2c9de3b4b4374d37cd80e3c8e7
Armeen Hadizadeh <armeen.hadizadeh@gmail.com>
2021-01-06 13:59:30 -0500

    added the countLetters function

---
+++ countLetters.js
@@ -0,0 +1,22 @@
+const assertEqual = (actual, expected) => {
+  actual === expected ? console.log(`🤑🤑🤑Assertion Passed: ${actual} === ${expected}`) :
+    console.log(`😡😡😡Assertion Failed: ${actual} !== ${expected}`);
+};
+
+const countLetters = function(string){
+  let noSpaces = string.split(' ').join('')
+
+  let obj = {};
+
+  for(let value of noSpaces){
+    if(obj[value]){
+      obj[value] += 1;
+    }else{
+      obj[value] = 1
+    };
+  };
+  
+  return obj
+};
+
+console.log(countLetters('lighthouse in the house'))
--- middle.js
+++ middle.js
@@ -2,31 +2,31 @@
 //for odd only one middle element should be returned 
 //for even numbers both middle elements should be returned 
 
-const eqArrays = function(array1, array2){
-  return array1.length === array2.length && array1.every((item, i, arr)=>{
+const eqArrays = function (array1, array2) {
+  return array1.length === array2.length && array1.every((item, i, arr) => {
     return item === array2[i]
   })
 }
 
-const assertArrayEquals = function(array1, array2) {
-  if(eqArrays(array1, array2)){
+const assertArrayEquals = function (array1, array2) {
+  if (eqArrays(array1, array2)) {
     console.log(`🤑🤑🤑Assertion Passed: ${array1} === ${array2}`)
-  }else{
+  } else {
     console.log(`😡😡😡Assertion Failed: ${array1} !== ${array2}`);
   }
 }
 
-const middle = function(array){
+const middle = function (array) {
   let arrayLength = array.length
 
-  if(array.length < 3){
+  if (array.length < 3) {
     return []
   }
 
-  if(array.length % 2 === 0){
-    return array.slice(arrayLength/2 -1, arrayLength/2 + 1)
-  }else{
-    return array.slice(Math.floor(arrayLength/2), Math.floor(arrayLength/2) + 1 )
+  if (array.length % 2 === 0) {
+    return array.slice(arrayLength / 2 - 1, arrayLength / 2 + 1)
+  } else {
+    return array.slice(Math.floor(arrayLength / 2), Math.floor(arrayLength / 2) + 1)
   }
 }
 
