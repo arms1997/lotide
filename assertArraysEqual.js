d3b291d51a227e861e3218b03e888b96b123950d
arms1997 <armeen.hadizadeh@gmail.com>
2021-01-06 01:10:07 +0000

    updated assertArraysEqual.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -8,22 +8,20 @@
 //   console.log(`😡😡😡Assertion Failed: ${array1} !== ${array1}`);
 // }
 
-const eqArrays = function(array1, array2) {
-  if(array1.length !== array2.length){
+const eqArrays = function(array1, array2){
+  return array1.length === array2.length && array1.every((item, i, arr)=>{
+    return item === array2[i]
+  })
+}
+
+const assertArrayEquals = function(array1, array2) {
+  if(eqArrays(array1, array2)){
+    console.log(`🤑🤑🤑Assertion Passed: ${array1} === ${array2}`)
+  }else{
     console.log(`😡😡😡Assertion Failed: ${array1} !== ${array2}`);
-    return; 
   }
-
-  for(let item in array1){
-    if(array1[item] !== array2[item]){
-      console.log(`😡😡😡Assertion Failed: ${array1} !== ${array2}`);
-      return;
-    }
-  }
-
-  console.log(`🤑🤑🤑Assertion Passed: ${array1} === ${array2}`)
 }
 
 
 
-eqArrays([1, 2, 3], [1, 2, 3, 4])
+assertArrayEquals([1, 2, 3], [1, 2, 3, 4])
