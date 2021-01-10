b220e5ed3fcdc70e5349712a5ff771e90f2d419d
arms1997 <armeen.hadizadeh@gmail.com>
2021-01-10 00:43:53 +0000

    added a recursive method to check if nested arrays are equal

--- eqArrays.js
+++ eqArrays.js
@@ -9,7 +9,31 @@ const eqArrays = function(array1, array2){
   })
 }
 
-console.log(eqArrays([1, 2, 3], [1, 2, '3']))
+const eqArrays2 = function(array1, array2){
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([3, 0, 4, '1', '2'], [3, 0, 4, '1', 2]), true);
+  if(array1.length !== array2.length){
+    return false
+  }
+
+  for(let value in array1){
+    if(Array.isArray(array1[value])){
+      if(!eqArrays2(array1[value], array2[value])){
+        return false
+      }
+    }else if(array1[value] !== array2[value]){
+      return false
+    }
+  }
+
+  return true
+}
+
+assertEqual(eqArrays2([1, 2, 3], [1, 2, '3']), false)
+
+assertEqual(eqArrays2([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays2([3, 0, 4, '1', 2], [3, 0, 4, '1', 2]), true);
+
+assertEqual(eqArrays2([[2, 3], [4]], [[2, 3], [4]]), true) // => true
+
+assertEqual(eqArrays2([[2, 3], [4]], [[2, 3], [4, 5]]), false) // => false
+assertEqual(eqArrays2([[2, 3], [4]], [[2, 3], 4]), false) // => false
